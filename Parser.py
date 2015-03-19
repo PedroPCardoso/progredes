c810dcdff4b3e343f9bdee524063d559fdae0e10
Pedro Mietro <pedroecomp@gmail.com>
2015-03-19 19:05:38 -0300

    Funcao de tirar caracteres especiais e comentarios atualizados

--- Parser.py
+++ Parser.py
@@ -1,32 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import urllib
 from bs4 import BeautifulSoup
 
 url = "http://www.uefs.br/portal"
-html = urllib.urlopen(url).read()
+
+meta =urllib.urlopen(url) 
+html = urllib.urlopen(url).read() # Lendo a url e passando o html
 soup = BeautifulSoup(html)
 
+#print meta.info()
+print soup.find_all('a')
+
+def getTamanho():
+	meta = url.info()
+	print "Content-Length:", meta.getheaders("Content-Length")[0] # retornando tamanho
+
 titulo = soup.title.text 
-print titulo
+#print titulo
+
 
 def getTitle():
 	titulo = soup.title.text 
 	titulo = titulo.lower() # Colocando em minusculo
 	return titulo
 
-# kill all script and style elements
-for script in soup(["script", "style"]):
-    script.extract()    # rip it out
 
-# get text
+for script in soup(["script", "style"]):
+	script.extract()    # Tirando os scripts do html
+
+# Pego todo texto sem os scripts
 text = soup.get_text()
 # print text 
-# break into lines and remove leading and trailing space on each
+''' Quebra a linha  e remove os espacos da direita e esquerda, 
+a funcao strip que remove esses espaços em branco'''
 lines = (line.strip() for line in text.splitlines())
 # break multi-headlines into a line each
 chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 # drop blank lines
 text = '\n'.join(chunk for chunk in chunks if chunk) # pular linha
 
+''' Fazer uma funcao usando o BeautifulSoup como eu posso pedir 
+pra ele retornar uma parte do codigo html que tem determinada tag
+e apartir do retorno dessa lista a gente faz um for que sai comparando se a nossa palavra 
+esta no meio dessas.
+  '''
 #print text
+text.encode('utf-8')
 print type(text)
+
+
+def remover_acentos(txt, codif='utf-8'):
+    txt = txt.lower() #colocando em minusculo
+    ''' Devolve copia de uma str substituindo os caracteres
+       acentuados pelos seus equivalentes não acentuados.  '''
+    return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
+
+ 
+'''def contagem(lista[]):
+	for i in lista[]:
+		for j in lista[]:
+			if i == j:
+				t+=1
+
+	return t
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
 
