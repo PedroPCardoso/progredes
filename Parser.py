7e30b935aaefb299573ea4940102d8bbfca730e7
Pedro Mietro <pedroecomp@gmail.com>
2015-03-21 21:30:59 -0300

    Coloquei alguns metodos em funções pra a poder ver melhor o código. Ta ficando massa kkk

--- Parser.py
+++ Parser.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import urllib
 from bs4 import BeautifulSoup
-
+from unicodedata import normalize
 url = "http://www.uefs.br/portal"
 
 meta =urllib.urlopen(url) 
@@ -23,6 +23,7 @@ titulo = soup.title.text
 def getTitle():
 	titulo = soup.title.text 
 	titulo = titulo.lower() # Colocando em minusculo
+	print titulo
 	return titulo
 
 
@@ -49,57 +50,66 @@ esta no meio dessas.
 text.encode('utf-8')
 print type(text)
 
-
+# funcao que remove os acentos de palavras
 def remover_acentos(txt, codif='utf-8'):
     txt = txt.lower() #colocando em minusculo
     ''' Devolve copia de uma str substituindo os caracteres
        acentuados pelos seus equivalentes não acentuados.  '''
     return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
 
- 
-'''def contagem(lista[]):
-	for i in lista[]:
-		for j in lista[]:
-			if i == j:
-				t+=1
-
-	return t
-
-'''
 
 
 
+def List_Palavras(text):
 
-
-
-lista=[]
-palavra = ""
-l = [te.encode('UTF8') for te in text] # Transformando o texto um uma lista de unicode utf8
+	lista=[]
+	palavra = ""
+	l = [te.encode('UTF8') for te in text] # Transformando o texto um uma lista de unicode utf8
 
 #print text
-for t in l:  #transformando em uma lista de string
+	for t in l:  #transformando em uma lista de string
 				
-		x = str(t)
+			x = str(t)
 
-		lista.append(t)
+			lista.append(t)
 
 
-print type (lista[23])	
+	lista2=[]
+	palavra=""
 
-lista2=[]
-palavra=""
+	for v in range(1,len(lista)): # CRIANDO FUCK LISTA DE PAALAVRAS
+		z= lista[v]
 
-for v in range(1,len(lista)): # CRIANDO FUCK LISTA DE PAALVRAS
-	z= lista[v]
+		if z.isspace():
+			lista2.append(palavra)
+			palavra=""
 
-	if z.isspace():
-		lista2.append(palavra)
-		palavra=""
-
-	else:
+		else:
 		#print z
-		palavra+=z
+			palavra+=z
+	return lista2
+# print lista2 caso queira printa a lista de todas as palavras
 
-print lista2
+def contas_Palavras(lista2):
+	lista3=[]
+	quant=0 # Funcao contar as palavras
+	for v in range(1,len(lista2)):
+		for t in range(2,len(lista2)):
+			if lista[v] == lista[t]:
+				quant += 1
 
+	print quant
 
+#List_Palavras(text) como chamar uma funcao
+getTitle()
+
+# Ta meio sem logica, so to fazendo por enquanto, mas tem que ajeitar esse codigo, 2 funcoes pra fazer uma coisa so :P
+#Funcao que printa todas as palavras sem o acento
+def sema_cento(): 
+	lista2 = List_Palavras(text)
+	lista_semacent=[]
+	for i in lista2: # asdiciona as palavras sem acento dentro de uma nova lista
+		lista_semacent.append(remover_acentos(i))
+	print lista_semacent
+
+sema_cento()
