275a3b3fce2018d49a0e29b00a6a85c098a0c6fe
manumacedo <manuecomp@yahoo.com.br>
2015-05-12 15:32:55 -0300

    funfou*-*

--- CGI/ServidorPython/cgi-bin/form.py
+++ CGI/ServidorPython/cgi-bin/form.py
@@ -9,43 +9,22 @@ nome = form["nome"].value
 comentario = form["coment"].value
 data = datetime.now()
 
-savingComents(nome,comentario,data)
-htmlOut()
+arquivo = open('coments.txt', 'r') #abrindo o arquivo pra leitura
+texto = arquivo.readlines() #lê o arquivo todo pra pegar a ultima linha e add numa list
+texto.append('Nome:' + nome)  #adiciona o nome do novo coment
+texto.append('Data:' + str(data))  #adiciona a data do novo coment
+texto.append('Comentário:' + comentario)  #adiciona o coment do novo coment
+#texto.append('\n') # pra detereminar o fim de um coment
+arquivo = open('coments.txt', 'w') # abre o arquivo pra escrita e adiciona a list com o novo coment
+arquivo.writelines(texto) 
+arquivo.close()
 
+arquivo2 = open('coments.txt', 'r') #abrindo o arquivo pra leitura
+allComents = arquivo2.readlines() # lê todas as linhas do arquivo e coloca em uma lista e cada linha é uma posição da lista
 
-def htmlOut():
-	arquivo = open('coments.txt', 'r') #abrindo o arquivo pra leitura
-	allComents = arquivo.readlines() # lê todas as linhas do arquivo e coloca em uma lista e cada linha é uma posição da lista
-
-	print "Content-type: text/html; charset=utf-8"
-	print
-	print "<html><head><title>Comentários</title></head><body>"
-	# for x in xrange(1,10):
-	print allComents + "<br>" # tem que ler todas as linhas 
-	print "</body></html>"
-
-def savingComents(nome, comentario, data):
-	arquivo = open('coments.txt', 'r') #abrindo o arquivo pra leitura
-	texto = arquivo.readlines() #lê o arquivo todo pra pegar a ultima linha e add numa list
-	texto.append('Nome:' + nome)  #adiciona o nome do novo coment
-	texto.append('Data:' + data)  #adiciona a data do novo coment
-	texto.append('Comentário:' + comentario)  #adiciona o coment do novo coment
-	texto.append('\n') # pra detereminar o fim de um coment
-	arquivo = open('coments.txt', 'w') # abre o arquivo pra escrita e adiciona a list com o novo coment
-	arquivo.writelines(texto) 
-	arquivo.close()
-	return arquivo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print "Content-type: text/html; charset=utf-8"
+print
+print "<html><head><title>Comentários</title></head><body>"
+# for x in xrange(1,10):
+print str(allComents) + "<br>" # tem que ler todas as linhas 
+print "</body></html>"
